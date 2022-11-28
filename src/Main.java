af6862bfd0cf8811915766c01b8740d909058a9b
HW4 <aliamachka@hotmail.com>
2022-11-28 15:05:36 -0800

    homewrok done 5

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        ///Problem1
+        System.out.println("Problem 1");
+
+        int clientOs = 0;
+        if (clientOs <= 0) {
+
+            System.out.println(" Установите версию приложения для iOS по ссылке");
+        } else if (clientOs >= 0) {
+            System.out.println("Установите версию приложения для Android по ссылке ");
+        }
+
+                ////Problem2
+                System.out.println("Problem 2 ");
+
+                int clientDeviceYear = 2015;
+                if (clientDeviceYear <= 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+
+
+            }
+
+
+        }
+
